69312a70d9fbac01fd15510b8a2f94c245967477
akcoder9 <94218789+akcoder9@users.noreply.github.com>
2021-11-13 14:16:25 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,207 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Love You Zindagi.",
+    artist: "",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Banjaara.",
+    artist: "",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Come and Get Your Love.",
+    artist: "",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Ik Vaari Aa.",
+    artist: "",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Ae Dil Hai Mushkil.",
+    artist: "",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Aladdin.",
+    artist: "",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Aladdin - Friend Like Me.",
+    artist: "",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Aladdin-Clip.",
+    artist: "",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Baatein Ye Kabhi Na.",
+    artist: "",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Here I Come.",
+    artist: "",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Behti Hawa Sa Tha Woh.",
+    artist: "",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Besabriyaan.",
+    artist: "",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "BULLEYA.",
+    artist: "",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Channa Mereya.",
+    artist: "",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Crazy-Frog.",
+    artist: "",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Alors on Danse.",
+    artist: "",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Hawayein.",
+    artist: "",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Mi Gente.",
+    artist: "",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "Sorry.",
+    artist: "",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Mile Ho Tum Humko.",
+    artist: "",
+    img: "music-20",
+    src: "music-20"
+  },
+  {
+    name: "Naina.",
+    artist: "",
+    img: "music-21",
+    src: "music-21"
+  },
+  {
+    name: "One Two One Two Dance.",
+    artist: "",
+    img: "music-22",
+    src: "music-22"
+  },
+  {
+    name: "Pal Pal Dil Ke Paas.",
+    artist: "",
+    img: "music-23",
+    src: "music-23"
+  },
+  {
+    name: "Sunflower.",
+    artist: "",
+    img: "music-24",
+    src: "music-24"
+  },
+  {
+    name: "Sunn Raha Hai Na Tu.",
+    artist: "",
+    img: "music-25",
+    src: "music-25"
+  },
+  {
+    name: "Tera Yaar Hoon Main.",
+    artist: "",
+    img: "music-26",
+    src: "music-26"
+  },
+  {
+    name: "Tere Jaisa Yaar Kahan.",
+    artist: "",
+    img: "music-27",
+    src: "music-27"
+  },
+  {
+    name: "Thor Ragnarok - Theme Song.",
+    artist: "",
+    img: "music-28",
+    src: "music-28"
+  },
+  {
+    name: "Tum Hi Ho.",
+    artist: "",
+    img: "music-29",
+    src: "music-29"
+  },
+  {
+    name: "Faded.",
+    artist: "",
+    img: "music-30",
+    src: "music-30"
+  },
+  {
+    name: "Ye Mosam Ki Baarish Ye Baarish Ka Pani.",
+    artist: "",
+    img: "music-31",
+    src: "music-31"
+  },
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
